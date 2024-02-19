44998f20ac51a3720168528c5d34bff28bf7d010
brenoxs99 <brenosam22@gmail.com>
2024-02-19 12:31:03 -0300

    Adicionando Arquivos

---
+++ Exercicio2.js
@@ -0,0 +1,10 @@
+//Crie um programa que calcule a idade do usuário. O
+//usuário deve digitar o ano de nascimento e o programa
+//deve exibir quantos anos ele tem.
+
+let name = prompt("Digite seu nome: ");
+let anoNasc = prompt("\nDigite o ano que você nasceu: ");
+let anoAtual = new Date().getFullYear(); // Funçao em "Js" que retorna o ano atual com 4 digitos.
+
+let idadeUsu = anoAtual - anoNasc;
+console.log("A sua idade é: " + idadeUsu);
---
+++ Exercício1.js
@@ -0,0 +1,7 @@
+//Crie um programa que solicite o nome do usuário e
+//depois exiba uma mensagem de saudação: 
+//“Olá [nome do usuário], seja bem-vindo ao mundo da programação!”
+
+let name = prompt("Digite seu nome: ");
+
+console.log("Olá " + name + " Seja bem vindo ao mundo da programação");
