3fa987395a9e38921d105db86f58139e1f0bd720
brenoxs99 <brenosam22@gmail.com>
2024-02-27 00:24:48 -0300

    Adicionado Exercício

---
+++ Exercicio4.js
@@ -0,0 +1,23 @@
+//Crie um programa que determine se um número é positivo, negativo ou zero. 
+//Solicite um número ao usuário e exiba o resultado.
+
+//Use o módolo "readLine" se estiver rodando seus códigos em Node.js
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Digite um número para verificar se ele é (NEGATIVO), (POSITIVO) ou (0): ", (NUM) => {
+    if(NUM > 0){
+        console.log("\nO número informado é POSITIVO!");
+    }
+    else if(NUM < 0){
+        console.log("\nO número informado é NEGATIVO!");
+    }else{
+        console.log("\nO número informado é igual a 0!");
+    }
+    
+  rl.close();
+});
