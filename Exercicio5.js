82625edc143bcc38e4e87640d506859f5913c482
brenoxs99 <brenosam22@gmail.com>
2024-02-29 00:11:00 -0300

    Add Exercício 5

---
+++ Exercicio5.js
@@ -0,0 +1,14 @@
+//Exercicio_5
+//Crie um programa que verifique se um número é par ou
+//ímpar. Solicite um número ao usuário e exiba o resultado.
+
+
+let num = prompt("Digite um numero: ");
+if((num % 2 == 0) && (num != 0)){
+    console.log("\nO numero digitado é Par!");
+}
+else if(num % 2 != 0){
+    console.log("\nO número digitado é Ímpar!")
+}else{
+    console.log("O número digitado é 0, ele é neutro!");
+}
